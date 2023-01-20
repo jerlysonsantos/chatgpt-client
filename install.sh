c934283af31d94c397cde91f77011bc2a5d1ece9
jerlysonsantos <jerlyson.santos@somosmesha.com>
2023-01-20 13:25:44 -0300

    chatgpt key requiment removed

--- install.sh
+++ install.sh
@@ -6,15 +6,6 @@ then
     exit
 fi
 
-if [ -z "$1" ]
-then
-    echo "[!] Please insert a chatgpt api key"
-    exit
-fi
-
-echo "CHATGPT_API_KEY=$1" >> $HOME/.bashrc
-echo "CHATGPT_API_KEY=$1" >> $HOME/.bash_profile
-
 pip install openai
 
 curl -L -o ~/.local/bin/chatgpt https://raw.githubusercontent.com/jerlysonsantos/chatgpt-client/main/chatgpt.py 
